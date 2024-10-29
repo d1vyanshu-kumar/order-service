439fba1f5f44d052330aa432f8da0e147c49bf08
divyanshu_Kumar <154233802+d1vyanshu-kumar@users.noreply.github.com>
2024-10-29 13:16:35 +0530

    Inter-Service Communication(order-service and inventory-service) and Testing with Wiremock

--- src/main/java/com/divyanshuLean/Microservices/order/OrderServiceApplication.java
+++ src/main/java/com/divyanshuLean/Microservices/order/OrderServiceApplication.java
@@ -2,8 +2,10 @@ package com.divyanshuLean.Microservices.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients // this enables the auto-configuration of necessary classes to use openfeign in our order service project
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
