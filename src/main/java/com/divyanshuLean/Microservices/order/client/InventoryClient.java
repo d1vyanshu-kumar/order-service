6a5a2606d652a5cba0089fdd1caa85d3011eb5f6
divyanshu_Kumar <154233802+d1vyanshu-kumar@users.noreply.github.com>
2024-10-31 19:46:25 +0530

    Document REST API with Open API and Refactoring

--- src/main/java/com/divyanshuLean/Microservices/order/client/InventoryClient.java
+++ src/main/java/com/divyanshuLean/Microservices/order/client/InventoryClient.java
@@ -1,15 +1,15 @@
 package com.divyanshuLean.Microservices.order.client;
 
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 
-@FeignClient(value = "inventory",url = "${inventory.url}") // this is the url where the inventory service is running
+//@FeignClient(value = "inventory",url = "${inventory.url}") // this is the url where the inventory service is running //no used library
+
 public interface InventoryClient {
     // so what kind of the method this http client will call we are defining here
     //getMethod
-    @RequestMapping(method = RequestMethod.GET, value = "")
+    //@RequestMapping(method = RequestMethod.GET, value = "")//no used library
+    @GetExchange("/api/inventory") // this is the url where the inventory service is running
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
     // similar like spring data jpa repository in which we have to only extend and use their method in the implemented class
     // now go to the order service class and inject the InventoryClient
